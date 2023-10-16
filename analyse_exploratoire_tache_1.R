3984b1b5bff3d452b8f2559df93d25ea271d1773
tokosel <abdoulayesallboke@outlook.fr>
2023-10-16 21:00:49 +0000

    analyse_exploratoire_tache_2

--- analyse_exploratoire_tache_1.R
+++ analyse_exploratoire_tache_1.R
@@ -1,5 +1,15 @@
-# Importation des données avec une URL
+# Tache 1
 
+#Importation et chargement des données
 url = 'https://github.com/JosueAfouda/Credit-Risk-Modeling/raw/master/data_credit.txt'
 
 df <- read.csv(url)
+
+str(df)
+
+install.packages("summarytools")
+# Charger les packages nécessaires
+library(summarytools)
+library(dplyr)
+# Utiliser dfSummary avec l'opérateur %>%
+dfSummary(df) %>% view()
